23299600e2cbd3a60abe68b948185b8cd926b32f
Hironao OTSUBO <motemen@gmail.com>
2022-07-03 00:36:24 +0900

    gh-pages config

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const { defineConfig } = require('vite');
+
+module.exports = defineConfig({
+  root: './demo',
+  build: {
+    outDir: path.join(__dirname, 'dist/demo'),
+    lib: {
+      entry: path.join(__dirname, 'src', 'index.ts'),
+      formats: ['es'],
+    },
+    rollupOptions: {
+      input: path.join(__dirname, 'demo', 'index.html'),
+    },
+  },
+});
